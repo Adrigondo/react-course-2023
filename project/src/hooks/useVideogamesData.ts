97814b332329eee0c122a0c48ee726c85566b9f5
Adrian González <adrigondo.profesional@gmail.com>
2023-10-23 15:03:40 -0600

    Update: fetchVideogames

---
+++ project/src/hooks/useVideogamesData.ts
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+/* API: https://rawg.io/apidocs */
+
+class VideogamesParams{
+  'page':number=1;
+  'size_page':number=10;
+}
+const useVideogamesData = () => {
+  const [games, setGames] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<unknown>("");
+
+  const fetchVideogames = async (params?: VideogamesParams) => {
+    setIsLoading(true);
+    try {
+      let stringParamsArray:Array<string>=[];
+      if(params){
+        stringParamsArray=Object.entries(params).map(([key, value])=>{
+          return `${key}=${value}`
+        });
+      }
+      let stringParams=stringParamsArray.join('&');
+      if(stringParams!=''){
+        stringParams='&'+stringParams;
+      }
+      const res = await fetch(`https://api.rawg.io/api/games?key=e5190f36da374820b8ec07396096d1c8${stringParams}`);
+      const data = await res.json();
+      setGames(data.results);
+      
+      setIsLoading(false);
+      setError(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+      setError(error);
+    }
+  }
+
+  return {
+    games,
+    error,
+    isLoading,
+    fetchVideogames
+  };
+}
+export default useVideogamesData;
