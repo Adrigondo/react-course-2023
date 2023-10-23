030b14f53ebb589bbd1d9576425f5b8f009b2dc3
Adrian González <adrigondo.profesional@gmail.com>
2023-10-23 14:25:14 -0600

    Update: class 23-october

--- project/src/views/Home/Home.tsx
+++ project/src/views/Home/Home.tsx
@@ -1,12 +1,21 @@
 import Header from "src/components/Header/Header";
 import GamesList from "../GamesList/GamesList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useVideogamesData from "src/hooks/useVideogamesData";
 
 const Home = () => {
     const [search, setSearch] = useState('');
+    const {games, fetchVideogames} = useVideogamesData();
+    console.log(games);
+
+    useEffect(() => {
+        fetchVideogames('&page_size=30&page=1');
+    }, [])
+    
     const handleChange = (event: any) => {
         setSearch(event.target.value)
         console.log(search);
+        fetchVideogames(`&search=${search}`);
     }
     return (
         <div>
@@ -18,7 +27,7 @@ const Home = () => {
                     Home
                 </h1>
             </Header>
-            <GamesList search={search}></GamesList>
+            <GamesList search={search} games={games}></GamesList>
         </div>
     );
 }
